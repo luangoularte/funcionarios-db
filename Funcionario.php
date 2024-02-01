16f494fc8422cc66b1cfdbfedaba2c247938b70e
luangoularte <luan.ag2012@hotmail.com>
2024-01-31 21:59:19 -0300

    att

---
+++ Funcionario.php
@@ -0,0 +1,40 @@
+<?php 
+
+class Funcionario {
+
+    protected int $id;
+    protected string $nome;
+    protected string $genero;
+    protected string $idade;
+    protected string $salario;
+
+    public function __construct()
+    {
+        
+    }
+
+    public function Create(){
+
+    }
+
+    public function ReadAll(){
+    
+    }
+
+    public function ReadId($id){
+
+    }
+
+    public function Update(){
+
+    }
+    
+    public function Delete(){
+
+    }
+
+    public function aumentaSalario($percentual){
+
+    }
+    
+}
